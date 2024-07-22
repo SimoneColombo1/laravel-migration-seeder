b5d2056b0d7f5114247fe3de25b99d402c7534b5
SimoneColombo1 <simcolombo2@gmail.com>
2024-07-22 16:31:32 +0200

    Creato Train Seeder

---
+++ database/seeders/TraiSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TraiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
